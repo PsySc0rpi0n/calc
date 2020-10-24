a7c3b398fbe501d4b21530ce2037b55b285ce07d
PsySc0rpi0n <totallacc@gmail.com>
2020-10-24 14:19:40 +0100

    Fixing issue-1

--- src/div.c
+++ src/div.c
@@ -1,1 +1,3 @@
-float divFunc(float pVar1, float pVar2);
+float divFunc(float pVar1, float pVar2){
+    return pVar1 / pVar2;
+}
--- src/div.h
+++ src/div.h
@@ -1,3 +1,1 @@
-float divFunc(float pVar1, float pVar2){
-    return pVar1 / pVar2;
-}
+float divFunc(float pVar1, float pVar2);
